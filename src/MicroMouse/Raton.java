a6ddbbf455098c009bc33864fb9b520c6d43708c
emiiv <emilio.lopez@gmail.com>
2023-11-08 06:40:51 -0600

    testing

--- src/MicroMouse/Raton.java
+++ src/MicroMouse/Raton.java
@@ -1,8 +1,11 @@
 package MicroMouse;
 
+import javax.swing.text.Style;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Raton {
     private char[][] mapaOriginal;
@@ -36,15 +39,23 @@ public class Raton {
                 lista.remove(i);
             }
         }
+        System.out.println("Los vecinos validos son:");
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }
         return lista;
     }
     public Punto encontrarMejorPaso(Punto currentPos){
         List<Punto> vecinos = this.getVecinos(currentPos);
+        currentPos = vecinos.get(0);
         for (int i = 0; i < vecinos.size(); i++) {
             if (vecinos.get(i).getManhattanDistance(fin) < currentPos.getManhattanDistance(fin)) {
                 currentPos = vecinos.get(i);
+                System.out.println("Nuevo vecino elegido");
             }
         }
+        System.out.println("El mejor siguiente paso es: ");
+        System.out.println(currentPos);
         return currentPos;
     }
     public List<Punto> encontrarMejorCamino() {
@@ -56,6 +67,9 @@ public class Raton {
         while(this.mapaOriginal[current.getY()][current.getX()] != 'F'){
             System.out.println("Valor flood original:");
             System.out.println(mapaFlood[current.getY()][current.getX()]);
+            mapaFlood[current.getY()][current.getX()]++;
+            System.out.println("Nuevo valor flood: ");
+            System.out.println(mapaFlood[current.getY()][current.getX()]);
             List<Punto> vecinos = getVecinos(current);
             if(vecinos.size() == 1){
                 System.out.println("PUNTO MUERTOOOO");
@@ -63,12 +77,11 @@ public class Raton {
             }
             siguiente = encontrarMejorPaso(current);
             this.listaDeMovimientos.add(new Punto(siguiente));
-            mapaFlood[current.getY()][current.getX()]+=5;
-            System.out.println("Nuevo valor flood: ");
-            System.out.println(mapaFlood[current.getY()][current.getX()]);
             current = siguiente;
             System.out.println("El raton se mueve a: ");
             System.out.println(current);
+            Scanner scanner = new Scanner(System.in);
+            scanner.nextLine();
         }
         //TODO encontrarMejorCamino
         return listaDeMovimientos;
